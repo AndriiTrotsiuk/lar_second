01c8ac9f88beee53e26df996ed2b746fecfc8bd5
Andrii Trotsiuk <and1reymail@gmail.com>
2023-08-15 19:34:00 +0300

    controller index action

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -19,7 +20,10 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+		return view('tasks.index', [
+			'tasks' => $tasks,
+		]);
     }
 
     /**
